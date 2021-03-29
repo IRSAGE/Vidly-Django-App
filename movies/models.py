5888859d36d77e67bc2ce28a92e3a9edc44ea036
IR~SAGE <sibo.egide98@gmail.com>
2021-03-29 22:34:48 +0200

    Adding a date_created Field to the Movies Table

--- movies/models.py
+++ movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -11,4 +12,5 @@ class Movie(models.Model):
     release_year = models.IntegerField()
     number_in_stock = models.IntegerField()
     daily_rate = models.FloatField()
-    genre = models.ForeignKey(Genre, on_delete= models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now)
