f40d3cc8c6661e22343803fc6b0bdbdb5bbcc15f
IR~SAGE <sibo.egide98@gmail.com>
2021-04-02 11:56:35 +0200

    Reduring Model Items on the html page

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    movies = Movie.objects.all()
+    # the above line equal to SELECT * FROM movies_movie
+    #return HttpResponse("Hello World")
+    return render(request, 'movies/index.html', {'movies':movies})
+
