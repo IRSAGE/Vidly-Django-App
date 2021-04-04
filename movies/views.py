a90400f72a7bac7516cb0231e0b31679649cbfd0
IR~SAGE <sibo.egide98@gmail.com>
2021-04-04 10:38:15 +0200

    Adding A view Movie Details Page

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Movie
 
 # Create your views here.
@@ -9,4 +9,9 @@ def index(request):
     # the above line equal to SELECT * FROM movies_movie
     #return HttpResponse("Hello World")
     return render(request, 'movies/index.html', {'movies':movies})
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie , pk=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
+
 
